4db471c46f8b0d374b098790fc689bc3ea2f9c2a
Jaime Burbano <burbanojaime98@gmail.com>
2019-08-15 11:55:56 -0500

    falta actualizar la interfaz

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import './reset.css';
 import './App.css';
 import CSVReader from 'react-csv-reader'
+
+var datos = [];
 
 export default class App extends React.PureComponent{
 
@@ -8,13 +11,16 @@ export default class App extends React.PureComponent{
     super(props);
     this.state={
       data: null,
+      filterData:null,
       uploadedData: false,
     }
   }
 
   uploadData = ( data ) => {
-    this.setState({data, uploadedData:true})
-    console.log(data);
+    data.shift();
+    datos = data;
+    this.setState({data, filterData:data ,uploadedData:true});
+    console.log(this.state.data);
   }
 
   showData = () => {
@@ -23,6 +29,26 @@ export default class App extends React.PureComponent{
     }else {
       return<p>{this.state.data}</p>
     }
+  }
+
+  ascendente = () => {
+    var arreglo = this.state.filterData;
+    arreglo = arreglo.sort( function(a, b) { 
+      return a[1] > b[1] ? 1 : -1;
+    });
+    
+    this.setState({filterData:arreglo});
+    console.log(this.state.filterData);
+  }
+
+  descendente = () => {
+    var arreglo = this.state.filterData;
+    arreglo = arreglo.sort( function(a, b) { 
+      return a[1] < b[1] ? 1 : -1;
+    });
+    
+    this.setState({filterData:arreglo});
+    console.log(this.state.filterData);
   }
 
   render(){
@@ -34,8 +60,38 @@ export default class App extends React.PureComponent{
           onFileLoaded={this.uploadData}
         />
 
-        {this.showData()}
-        
+        <section className="Table">
+          <article className="Table__title">
+            <p className="Table__title__text">Correo</p>
+            <p className="Table__title__text">Participaciones</p>
+            <p className="Table__title__text">Datos A</p>
+            <p className="Table__title__text">Datos B</p>
+          </article>
+
+          {
+           this.state.filterData ? datos.map( (item) => {
+                return(
+                  <article className="Table__item" key={item} >
+                    <p className="Table__item__text">{item[0]}</p>
+                    <p className="Table__item__text">{item[1]}</p>
+                    <p className="Table__item__text">{item[2]}</p>
+                    <p className="Table__item__text">{item[3]}</p>
+                  </article>
+                )
+              })
+                :
+                ''
+            }
+        </section>
+
+        <article className="Botones">
+          <button onClick={ () => this.ascendente()}>Filtrar participaciones de manera ascendente</button>
+          <button onClick={ () => this.descendente()}>Filtrar participaciones de manera descendente</button>
+        </article>
+
+
+        {//<button>Cargar archivo nuevamente</button> 
+        }     
       </div>
       
 
